103b45d2686522964d32dc4f2237c3b780733ae7
Schansge <grosser.knuff@googlemail.com>
2012-03-26 13:09:19 +0200

    node class got the same command sheme as routingNode

--- cluster/node.py
+++ cluster/node.py
@@ -10,6 +10,12 @@ class Node(Process):
         self.address = address
         self.routerAddress = address.split('.')[0]
 
+        # list of commands
+        self.commands = {}
+
+        # register standard commands
+        self.register_command('print', self.print_)
+
     def run(self):
         # main loop
         while True:
@@ -35,12 +41,35 @@ class Node(Process):
 
             else:
                 # call on message method
-                self.on_message(sender, message)
+                answer = self.on_message(sender, message)
+
+                # send answer
+                if not answer is None:
+                    self.output.put((self.address, sender, answer))
 
     def on_message(self, sender, message):
-        # output input
-        print '{} received data from {}: {}'.format(
-            self.address, sender, message)
+        # try execute command
+        try:
+            # check length of message
+            if len(message) == 1:
+                return self.commands[message[0]](sender)
+
+            elif len(message) == 2:
+                return self.commands[message[0]](sender, message[1])
+
+            else:
+                return self.commands[message[0]](sender, *message[1:])
+
+        except:
+            # answer
+            return ('unsupported command', message)
+
+    def register_command(self, command, callable):
+        # add command to dict
+        self.commands[command] = callable
+
+    def print_(self, sender, string):
+        print string
 
     def start(self, input, output):
         # save input and output
