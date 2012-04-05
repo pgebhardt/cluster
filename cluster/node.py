1897cda66e65cf1188aa9009c33935f9b2cf48ec
Schansge <grosser.knuff@googlemail.com>
2012-04-05 10:34:49 +0200

    new message style implemented in node

--- cluster/node.py
+++ cluster/node.py
@@ -10,114 +10,79 @@ class Node(Process):
         self.address = address
         self.routerAddress = address.split('.')[0]
 
-        # list of commands
-        self.commands = {}
-
         # list of responder
         self.responder = {}
 
-        # register standard commands
-        self.register_command('print', self.print_)
-        self.register_command('error', self.error)
+        # set running flag
+        self.running = True
 
     def run(self):
         # main loop
-        while True:
+        while self.running:
             # wait for input data
-            sender, reciever, message = self.input.recv()
+            message = self.input.recv()
 
-            # check for termination
-            if message[0] == 'stop':
-                # inform router to delete node
-                self.output.put((self.address, self.routerAddress,
-                    ('delete node', self.address)))
+            # check for correct receiver
+            if message['receiver'] != self.address:
+                # TODO
+                pass
 
-                # output termination
-                print 'terminating node {}'.format(self.address)
+            # check for request
+            if 'request' in message:
+                # call message handler
+                self.on_message(message)
 
-                # stop process
-                return
+            elif 'response' in message:
+                # call response handler
+                self.responder[(message['response'], message['sender'])](
+                    message['sender'], *message['args'], **message['kargs'])
 
-            # check correct reciever
-            if reciever != self.address:
-                self.output.put((self.address, sender,
-                    ('wrong reciever', message)))
+                # unregister responder
+                del self.responder[(message['response'], message['sender'])]
 
-            else:
-                # call on message method
-                answer = self.on_message(sender, message)
-
-                # send answer
-                if not answer is None:
-                    self.output.put((self.address, sender, answer))
-
-    def on_message(self, sender, message):
-        # check for responder
-        if (message[0], sender) in self.responder:
-            # execute responder
-            try:
-                # check length of message
-                if len(message) == 1:
-                    return self.responder[(message[0], sender)](sender)
-
-                elif len(message) == 2:
-                    return self.responder[(message[0], sender)](
-                        sender, message[1])
-
-                else:
-                    return self.responder[(message[0], sender)](
-                        sender, *message[1:])
-
-                # delete responder
-                del self.responder[(message[0], sender)]
-
-            except:
-                # send error message
-                return ('error', ('Executing responder', sender, message))
-
-        # check for commands
-        elif message[0] in self.commands:
-            # execute command
-                try:
-                    # check length of message
-                    if len(message) == 1:
-                        return self.commands[message[0]](sender)
-
-                    elif len(message) == 2:
-                        return self.commands[message[0]](sender, message[1])
-
-                    else:
-                        return self.commands[message[0]](sender, *message[1:])
-
-                except:
-                    # send error message
-                    return ('error', ('Executing command', message))
-
-        # unsupported command
-        else:
-            return ('unsupported command', message)
-
-    def register_command(self, command, callable):
-        # add command to dict
-        self.commands[command] = callable
-
-    def register_responder(self, command, sender, callable):
-        # add responder to dict
-        self.responder[(command, sender)] = callable
-
-    def request(self, receiver, request, response, responder):
+    def request(self, receiver, responder, request, *args, **kargs):
         # register responder
-        self.register_responder(response, receiver, responder)
+        self.register_responder(request, receiver, responder)
+
+        # generate message
+        message = {'request': request, 'sender': self.address,
+            'receiver': receiver, 'args': args, 'kargs': kargs}
 
         # send request
-        self.output.put((self.address, receiver, request))
+        self.output.put(message)
 
-    def print_(self, sender, string):
+    def respond(self, reciever, request, *args, **kargs):
+        # generate message
+        message = {'sender': self.address, 'reciever': reciever,
+            'response': request, 'args': args, 'kargs': kargs}
+
+        # send response
+        self.output.put(message)
+
+    def register_responder(self, request, reciever, responder):
+        # add responder to dict
+        self.responder[(request, reciever)] = responder
+
+    def on_message(self, message):
+        # get request
+        request = message['request']
+
+        # check for callable attribute
+        if hasattr(self, request):
+            # check for callable
+            if callable(getattr(self, request)):
+                # call method
+                response = getattr(self, request)(*message['args'],
+                    **message['kargs'])
+
+                # send response
+                self.respond(message['sender'],
+                    message['request'], response)
+
+    def print_to_screen(self, string):
         print string
 
-    def error(self, sender, message):
-        # output error
-        print '{}: Error: {}'.format(self.address, message)
+        return None
 
     def start(self, input, output):
         # save input and output
@@ -126,3 +91,7 @@ class Node(Process):
 
         # call base class method
         super(Node, self).start()
+
+    def stop(self):
+        # set running flag
+        self.running = False
